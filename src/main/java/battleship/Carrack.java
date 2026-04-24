c05dd7755607a208ac3304bb6e63dd3723936917
leonardo <lfdto@iscte-iul.pt>
2026-04-24 16:36:27 +0100

    Refactoring: Rename - variáveis de loop em Carrack

--- src/main/java/battleship/Carrack.java
+++ src/main/java/battleship/Carrack.java
@@ -23,20 +23,20 @@ public class Carrack extends Ship {
 
 		switch (bearing) {
 			case SOUTH:
-				for (int r = 0; r < this.getSize(); r++)
-					getPositions().add(new Position(pos.getRow() + r, pos.getColumn()));
+				for (int rowOffset = 0; rowOffset < this.getSize(); rowOffset++)
+					getPositions().add(new Position(pos.getRow() + rowOffset, pos.getColumn()));
 				break;
 			case NORTH:
-				for (int r = 0; r < this.getSize(); r++)
-					getPositions().add(new Position(pos.getRow() + r, pos.getColumn()));
+				for (int rowOffset = 0; rowOffset < this.getSize(); rowOffset++)
+					getPositions().add(new Position(pos.getRow() + rowOffset, pos.getColumn()));
 				break;
 			case EAST:
-				for (int c = 0; c < this.getSize(); c++)
-					getPositions().add(new Position(pos.getRow(), pos.getColumn() + c));
+				for (int colOffsset = 0; colOffsset < this.getSize(); colOffsset++)
+					getPositions().add(new Position(pos.getRow(), pos.getColumn() + colOffsset));
 				break;
 			case WEST:
-				for (int c = 0; c < this.getSize(); c++)
-					getPositions().add(new Position(pos.getRow(), pos.getColumn() + c));
+				for (int colOffset = 0; colOffset < this.getSize(); colOffset++)
+					getPositions().add(new Position(pos.getRow(), pos.getColumn() + colOffset));
 				break;
 		}
 	}
