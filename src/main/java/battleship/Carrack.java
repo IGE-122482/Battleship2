bbef425362cb17356d4567508bfe7a447184db6f
António <ankon@iscte-iul.pt>
2026-04-24 16:23:33 +0100

    Refactoring: Extract Method - addVerticalPositions/addHorizontalPositions em Carrack

--- src/main/java/battleship/Carrack.java
+++ src/main/java/battleship/Carrack.java
@@ -23,21 +23,22 @@ public class Carrack extends Ship {
 
 		switch (bearing) {
 			case SOUTH:
-				for (int r = 0; r < this.getSize(); r++)
-					getPositions().add(new Position(pos.getRow() + r, pos.getColumn()));
-				break;
 			case NORTH:
-				for (int r = 0; r < this.getSize(); r++)
-					getPositions().add(new Position(pos.getRow() + r, pos.getColumn()));
+				addVerticalPositions(pos);
 				break;
 			case EAST:
-				for (int c = 0; c < this.getSize(); c++)
-					getPositions().add(new Position(pos.getRow(), pos.getColumn() + c));
-				break;
 			case WEST:
-				for (int c = 0; c < this.getSize(); c++)
-					getPositions().add(new Position(pos.getRow(), pos.getColumn() + c));
+				addHorizontalPositions(pos);
 				break;
 		}
+	}
+	private void addVerticalPositions(IPosition pos) {
+		for (int r = 0; r < this.getSize(); r++)
+			getPositions().add(new Position(pos.getRow() + r, pos.getColumn()));
+	}
+
+	private void addHorizontalPositions(IPosition pos) {
+		for (int c = 0; c < this.getSize(); c++)
+			getPositions().add(new Position(pos.getRow(), pos.getColumn() + c));
 	}
 }
